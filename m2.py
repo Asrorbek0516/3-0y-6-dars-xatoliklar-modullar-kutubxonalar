5824c8ccacffa858a43ed12e2f39a47f11630ce1
Asrorbek0516 <asrorbekalimqulov@mail.com>
2026-07-11 10:36:13 +0500

    2-misol

---
+++ m2.py
@@ -0,0 +1,9 @@
+import math
+
+son = float(input("Son kiriting: "))
+
+yuqori = math.ceil(son)
+past = math.floor(son)
+
+print(f"{son} ning yuqoriga yaxlitlangan qiymati: {yuqori}")
+print(f"{son} ning pastga yaxlitlangan qiymati: {past}")
